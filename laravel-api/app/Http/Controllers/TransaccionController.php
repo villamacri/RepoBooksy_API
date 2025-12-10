f62f025e133fb72128ba48458e5993c5ac3c7039
Cristian Villalba <villalba.macri24@triana.salesianos.edu>
2025-12-10 14:25:00 +0100

    controllers hechos

---
+++ laravel-api/app/Http/Controllers/TransaccionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Transaccion;
+use Illuminate\Http\Request;
+
+class TransaccionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Transaccion $transaccion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Transaccion $transaccion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Transaccion $transaccion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Transaccion $transaccion)
+    {
+        //
+    }
+}
